283f8b0bda04c74bf0f8d5fa3ec4d80b29d767b5
Petro Rikabal Aviles <ricabalaviles@gmail.com>
2021-08-24 13:59:13 +0300

    Flex/Grig Ht implemented

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,6 +1,7 @@
-
 document.addEventListener("DOMContentLoaded", function() {
+
     const path = window.location.pathname.split('/').pop();
     const activeNav = document.querySelectorAll(`[href="${path}"]`)[0];
+
     activeNav.classList.toggle('nav__link--active')
 });
